dd66fedcc9478a1d3e167e8fa5c9885582593724
DuarteCamilo <cduartrivera@gmail.com>
2024-10-06 17:38:30 -0500

    notification entity adjusment

--- app/src/main/java/eam/edu/unieventos/model/Notification.kt
+++ app/src/main/java/eam/edu/unieventos/model/Notification.kt
@@ -4,8 +4,10 @@ import java.util.Date
 
 data class Notification(
     val id: String,
-    val clientId: String,
+    val from: String,
+    val to: String,
     val message: String,
+    val eventId: String,
     val sendDate: Date,
     val isRead: Boolean
 )
