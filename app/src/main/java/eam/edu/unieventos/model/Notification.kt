71f9421bf5a65b03602d26847ee19f257f690711
Joseph_MCV <91038457+JosephMCV@users.noreply.github.com>
2024-11-21 09:13:21 -0500

    add firebase constructor to notification

--- app/src/main/java/eam/edu/unieventos/model/Notification.kt
+++ app/src/main/java/eam/edu/unieventos/model/Notification.kt
@@ -3,12 +3,14 @@ package eam.edu.unieventos.model
 import java.util.Date
 
 data class Notification(
-    var id: String,
-    val from: String,
-    val to: String,
-    val message: String,
-    val eventId: String,
-    val sendDate: Date,
-    val isRead: Boolean
-)
+    var id: String = "", // Proporciona valores por defecto
+    val from: String = "",
+    val to: String = "",
+    val message: String = "",
+    val eventId: String = "",
+    val sendDate: Date = Date(),
+    val isRead: Boolean = false
+) {
+    // El constructor sin argumentos es automáticamente generado por Kotlin con valores por defecto
+}
 
